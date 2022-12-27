2cfc7e181fb3099b151e77538abb9c8a1c035128
Mertcan Coskun <38556356+mertcan79@users.noreply.github.com>
2022-12-27 22:36:44 +0100

    Update config.py

--- config.py
+++ config.py
@@ -12,15 +12,9 @@ class Settings(BaseSettings):
     PROJECT_VERSION: str = "1.0.0"
 
     USE_SQLITE_DB: str = False
-    POSTGRES_USER: str = 'postgres'
-    POSTGRES_PASSWORD = '123'
+    POSTGRES_USER: str = os.environ.get('PSQL_USER')
+    POSTGRES_PASSWORD = os.environ.get('PSQL_PASSWORD')
     POSTGRES_SERVER: str = 'localhost'
     POSTGRES_PORT: str = '5432'
-    POSTGRES_DB: str = 'db'
+    POSTGRES_DB: str = os.environ.get('PSQL_DB')
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
-    SECRET_KEY: str = '123'
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 240
-
-    TEST_USER_EMAIL = "test@example.com"

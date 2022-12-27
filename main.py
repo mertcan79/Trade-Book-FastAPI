08b46e830101aef61da2b5eba4cb301f1e9fdb38
Mertcan Coskun <38556356+mertcan79@users.noreply.github.com>
2022-12-28 00:09:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from models import ClientOrder
 from utils import TradeBook
 from typing import Dict
 from fastapi.middleware.cors import CORSMiddleware
-
+from datetime import datetime
 
 app = FastAPI(title='TRADE_BOOK', version='1.0')
 
@@ -17,16 +17,12 @@ app.add_middleware(
 
 
 @app.get('/')
-def home() -> Dict:
+def trade_history() -> Dict:
     connection, cursor = get_db_connection()
     # Load the initial positions and prices from the database
-    cursor.execute("SELECT * FROM positions")
-    initial_positions = {row[0]: row[1] for row in cursor.fetchall()}
-    cursor.execute("SELECT * FROM prices")
-    initial_market_prices = {row[0]: row[1] for row in cursor.fetchall()}
-
-    return {"message": 'Welcome to the trade book.', 'Initial Market Prices': initial_market_prices,
-            'Positions': initial_positions}
+    cursor.execute("SELECT * FROM trade_history")
+    history = cursor.fetchall()
+    return {"message": 'Welcome to the trade book.', 'History': history}
 
 
 @app.post("/trade")
@@ -61,12 +57,23 @@ def trade(order: ClientOrder) -> Dict:
     for instrument_id, price in trade_book.market_prices.items():
         cursor.execute("UPDATE prices SET price = %s WHERE instrument_id = %s", (price, instrument_id))
 
+    timestamp = str(datetime.now())
+    sharpe_ratio = trade_book.calculate_sharpe_ratio(risk_free_rate=0.03, time_period=1)
+    roi = trade_book.calculate_roi(instrument_id=order.instrument_id)
+    var = int(trade_book.calculate_var(alpha=0.95))
+    calmar = trade_book.calculate_calmar_ratio(time_period=1)
+
+    cursor.execute("INSERT INTO trade_history (instrument_id, position, timestamp, price) VALUES (%s, %s, %s, %s)",
+                   (order.instrument_id, order.quantity, timestamp, order.traded_price))
+
     connection.commit()
     cursor.close()
     connection.close()
 
-    return {'Instrument Traded': order.instrument_id, "P&L for the portfolio": trade_book.pnl, 
+    return {'Instrument Traded': order.instrument_id, "P&L for the portfolio": trade_book.pnl,
             'Max Drawdown': trade_book.maximum_drawdown,
             'Hedged?': str(order.hedge), 'Price': str(order.traded_price),
-            'Value at Risk': int(trade_book.calculate_var(alpha=0.95))
-            }
+            'Value at Risk': var,
+            'sharpe_ratio': sharpe_ratio,
+            'ROI': roi,
+            'Calmar Ratio': calmar}
